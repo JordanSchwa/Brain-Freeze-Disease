53aa83ea1dcb0584605a7c8061368b85a2753f60
JordanSchwa <53061031+JordanSchwa@users.noreply.github.com>
2019-07-29 19:08:29 -0700

    Add files via upload

---
+++ myInteractions.js
@@ -0,0 +1,19 @@
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.width = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
+
+/*
+function postss(){
+  return "<article class="center paragraphfiller morepadding">"
+    return "<h4>1: 7/_/19 [location name]</h4>"
+    return "<p>information about what our first location is.</p>"
+  return "</article>"
+
+}
+*/
